fdbc1aed27d09c004a38613bde13a4f65df24e36
Brendan O'Brien <bobrien@pursuit.org>
2024-03-16 18:30:03 -0400

    solves first problem with no added features

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -3,3 +3,8 @@ let currentColor = document.getElementById('current-color');
 
 for (let color of colors)
     color.addEventListener("click", (e) => currentColor.setAttribute("style", color.style.cssText));
+
+let cell = document.getElementsByClassName('cell');
+
+for (let i=0; i < cell.length; i++)
+    cell[i].addEventListener("click", (e) => cell[i].setAttribute("style", currentColor.style.cssText));
